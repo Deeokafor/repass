52f83e08d5309bfbbde905da3d3a90903919d726
Deeokafor <41968262+Deeokafor@users.noreply.github.com>
2019-07-12 21:45:53 +0100

    YAY! responsive
    built responsiveness into product's pages and features

--- scripts/engines.js
+++ scripts/engines.js
@@ -6,27 +6,47 @@ function engines(){
     var output = document.getElementById('output');
     var generateButton = document.getElementById('gen-btn');
     var message = document.getElementById('message-display');
+    var copy = document.getElementById('copy');
+    //var genPass = '';
 
     generateButton.addEventListener('click', randomizer1);
-    
-    function randomizer1(genPass){
+    copy.addEventListener('click', getContent);
 
+    //this function handles password generation
+    function randomizer1(){
         //data initialization
         var lettersOfAlpha = [
             'a', 'b', 'c', 'd', 'e', 'f','g', 'h', 'i', 'j', 'k',
             'l', 'm', 'n', 'o','p', 'q', 'r', 's', 't', 'u', 'v',
             'w', 'x','y', 'z'
         ];
-        
+
         //generated string will be passed into this variable
         var password = '';
-        
         for (var i = 0; i < 12; i++) {
             //findout why undefined is returned as part of string
             password += lettersOfAlpha[Math.round(Math.random(lettersOfAlpha) * lettersOfAlpha.length)];
         }
+
         console.log(password)
         output.value = password;
-        //return password;
+        genPass = password;
+        return genPass;
+    }
+
+    /*
+    if (genPass.includes(undefined)){
+        randomizer1();
+    }
+    */
+
+    //this function gets password genertated and copies to virtual clipboard
+    function getContent(){
+        if(output === ''){
+            message.innerHTML = 'you have no password generated yet, click the \' generate button';
+        } else {
+            message.innerHTML = 'password copied successfully!';
+            message.style.color = 'rgb(56, 128, 56)';
+        }
     }
 }
